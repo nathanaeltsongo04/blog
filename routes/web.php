46bce8883373448ed7a9aa150e7ed1c056f7f8d6
Nathanael <nathanaeltsongo04@gmail.com>
2024-12-02 10:08:12 +0100

    adding blog template and routing views

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
+})->name('welcome');
+Route::get('/post_detail', function () {
+    return view('pages.show');
 });
